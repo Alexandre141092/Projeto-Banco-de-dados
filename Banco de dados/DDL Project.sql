958f5634250c2e562be65b25c3fe6571651a97e0
marcobutrico <marcobutrico@gmail.com>
2025-04-07 21:56:41 -0300

    atualizando

--- Banco de dados/DDL Project.sql
+++ Banco de dados/DDL Project.sql
@@ -8,3 +8,33 @@ INSERT INTO Produto (Nome, Descricao, Preco, EstoqueDisponivel, Categoria, Image
 VALUES
 ('Mouse', 'Mouse Logitech', 99.90, 50, 'Informatica', ''),
 ('Teclado', 'Teclado Dell', 209.50, 100, 'Informatica', '')
+
+
+INSERT INTO Cliente (NomeCompleto, Email, Telefone, Endereco, DataCdastro)
+VALUES
+('Vinicio Santos', 'Vinicio@senai.br', '(11) 999944444', 'Rua niteroi 180, - sao paulo', '05/04/2022'),
+('Saulo santos', 'SAulo@senai.br', '(11) 25421104', 'Rua niteroi 250, - sao paulo', '05/05/2018')
+
+INSERT INTO Pedido (IdCliente, DataPedido, Status, ValorTotal)
+VALUES
+(1,'06/05/2023', 'Processando', 3200.99),
+(2,'10/05/2023', 'concluido', 450.99)
+
+INSERT INTO Pagamento (IdPedido, FormaPagamento, StatusPagamento, DataPagamnento)
+VALUES
+(1,'Cartao de credito', 'Aprovado', '08/05/2023 12:00:00'),
+(2, 'Boleto', 'Aprovado', '18/05/2023 16:30:00')
+
+INSERT INTO ItemPedido (IdPedido, IdProduto, Quantidade)
+VALUES
+(2,1,2),
+(2,2,1),
+(3,1,1) 
+
+--DELETE FROM Cliente WHERE NomeCompleto = 'Vinicio Santos';
+
+--DQL - comandos para visualizar os dados
+
+--Select * (ver tudo) ou "from" para ver o que for selecionado
+
+SELECT * FROM Cliente;
--- Banco de dados/DML project.sql
+++ Banco de dados/DML project.sql
@@ -1,48 +1,32 @@
-USE ECommerce;
+
+--DML - inserir dados, aletrar e apagar dados
+
+-- Insert into - inserir dados
+USE ECommerce
+
+INSERT INTO Produto (Nome, Descricao, Preco, EstoqueDisponivel, Categoria, Imagem)
+VALUES
+('Mouse', 'Mouse Logitech', 99.90, 50, 'Informatica', ''),
+('Teclado', 'Teclado Dell', 209.50, 100, 'Informatica', '')
 
 
+INSERT INTO Cliente (NomeCompleto, Email, Telefone, Endereco, DataCdastro)
+VALUES
+('Vinicio Santos', 'Vinicio@senai.br', '(11) 999944444', 'Rua niteroi 180, - sao paulo', '05/04/2022'),
+('Saulo santos', 'SAulo@senai.br', '(11) 25421104', 'Rua niteroi 250, - sao paulo', '05/05/2018')
 
--- Linguagem SQL
---SQL - Structured Query Language
---Linguagem de consulta estruturada
--- Banco de dados -> tabelas -> colunas
+INSERT INTO Pedido (IdCliente, DataPedido, Status, ValorTotal)
+VALUES
+(1,'06/05/2023', 'Processando', 3200.99),
+(2,'10/05/2023', 'concluido', 450.99)
 
--- T - SQL(Microsoft), PL/SQL (oracle) - tem if else, funcoes
+INSERT INTO Pagamento(IdPagamento, FormaPagamento, StatusPagamento, DataPagamnento)
+VALUES
+(1,'Cartao de credito', 'Aprovado', '08/05/2023 12:00:00'),
+(2, 'Boleto', 'Aprovado', '18/05/2023 16:30:00')
 
--- sql - comandos
--- DDL - Criar, altear ou apagar banco de dados e tabela
---DDL - data definition language
---Create
--- drop - apaga banco de dados ou tabela
-
-create database ECommerce;
-
-
-
--- drop database ECommerce
-
--- singular, comando letra maiuscula
-
-create table cliente (
--- PRIMARY KEY - coluna que identifica os cliente
--- IDENTITY - Gerada automaticamente
-   IdCliente INT PRIMARY KEY IDENTITY ,
-   NomeCompleto VARCHAR(150),
-   Email VARCHAR(100),
-   Telefone VARCHAR(20),
-   Endereco VARCHAR(255),
-   DataCdastro DATE,
-   );
-
-create table Pedido(
-   IdPedido INT PRIMARY KEY IDENTITY,
-   DataPedido DATE,
-   Status VARCHAR(20),
-   ValorTotal DECIMAL(18, 6),
-   IdCliente INT FOREIGN KEY REFERENCES Cliente (IdCliente),
-);
-
---DML - criar alterar ou apagar dados
---DML - data manipulation language
---DQL - ver os dados
---DQL - data query language
+INSERT INTO ItemPedido (IdPedido, IdProduto, Quantidade)
+VALUES
+(2,1,2),
+(2,2,1),
+(3,1,1) 
