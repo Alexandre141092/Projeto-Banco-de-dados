859ac3e78952e4a3bce8508d81633f674466c50c
marcobutrico <marcobutrico@gmail.com>
2025-04-17 22:00:55 -0300

    17/04

---
+++ Banco de dados/ECommerceAPI/ECommerceAPI/PagamentoRepository.cs
@@ -0,0 +1,63 @@
+﻿using EcommerceAPI.Context;
+using EcommerceAPI.interfaces;
+using EcommerceAPI.Models;
+
+namespace EcommerceAPI.Repository
+{
+    public class PagamentoRepository: IPagamentosRepository
+    {
+        private readonly EcommerceContext _context;
+
+        public PagamentoRepository(EcommerceContext context)
+        {
+            _context = context;
+        }
+
+        public void Atualizar(int id, Pagamento pagamento)
+        {
+            Pagamento pagamentoEncontrado = _context.Pagamentos.Find(id);
+
+            if (pagamentoEncontrado == null)
+            {
+                throw new Exception();
+            }
+
+          
+            _context.SaveChanges();
+
+        }
+        
+
+      
+        public Pagamento BuscarPOId(int id)
+        {
+              return _context.Pagamentos.FirstOrDefault(p =>p.IdPagamento == id);
+           
+        }
+
+        public void Cadastrar(Pagamento pagamento)
+        {
+            _context.Pagamentos.Add(pagamento);
+            _context.SaveChanges();
+        }
+
+        public void Deletar(int id)
+        {
+            Pagamento pagamentoEncontrado = _context.Pagamentos.Find(id);
+
+            if (pagamentoEncontrado == null)
+            {
+                throw new Exception();
+            }
+
+            _context.Pagamentos.Remove(pagamentoEncontrado);
+
+            _context.SaveChanges();
+        }
+
+        public List<Pagamento> ListarTodos()
+        {
+            return _context.Pagamentos.ToList();
+        }
+    }
+}
