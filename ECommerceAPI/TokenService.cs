e1e1642a23872a2acef93e3f828dc2f391e3709f
marcobutrico <marcobutrico@gmail.com>
2025-04-28 21:56:10 -0300

    Aula28/04

---
+++ ECommerceAPI/TokenService.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace EcommerceAPI.Services
+{
+    public class TokenService
+    {
+        //Claims - informacao do usuario que quero guardar
+        public string GenerateToken(string email)
+        {
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.Email, email)
+            };
+
+            // criar uma chave de seguranca e criptografarela
+            var chave = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("minha-chave-secreta-senai"));
+        }
+    }
+}
